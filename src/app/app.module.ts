773b0b200ca93ad57736ec51dbe6a5b1de7e185f
FreezyCGI <patrick.fritzl@gmx.at>
2021-12-27 16:23:45 +0100

    basic Angular structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { MenuItemListComponent } from './menu-item-list/menu-item-list.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuItemComponent,
+    MenuItemListComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu-item-list/menu-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item-list',
+  templateUrl: './menu-item-list.component.html',
+  styleUrls: ['./menu-item-list.component.css']
+})
+export class MenuItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

86d742e8866a109b311c4eb71ca28826af08a146
FreezyCGI <patrick.fritzl@gmx.at>
2021-12-27 19:11:30 +0100

    Added navigation links to <a>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuItemListComponent } from './menu-item-list/menu-item-list.component';
+import { ReviewsComponent } from './reviews/reviews.component'
 
 const routes: Routes = [
-  { path: 'menuItemList', component:MenuItemListComponent }
+  { path: '', redirectTo: '/menuItemList', pathMatch: 'full' },
+  { path: 'menuItemList', component:MenuItemListComponent },
+  { path: 'reviews', component:ReviewsComponent }
 ];
 
 @NgModule({

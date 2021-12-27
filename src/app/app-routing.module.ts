7187dc80c528543e55fe2a03c5514fcc84f82650
FreezyCGI <patrick.fritzl@gmx.at>
2021-12-27 17:12:21 +0100

    Routing-module & Refactoring
    Moved code from menu to app.component.
    created routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuItemListComponent } from './menu-item-list/menu-item-list.component';
 
-
+const routes: Routes = [
+  { path: 'menuItemList', component:MenuItemListComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }

460daadb5bbebb39797dcf8ac9867ecf33c8d543
FreezyCGI <patrick.fritzl@gmx.at>
2021-12-27 19:02:41 +0100

    Added buttons and component reviews

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
 import { MenuItemListComponent } from './menu-item-list/menu-item-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
     MenuItemComponent,
     MenuItemListComponent,
     ShoppingCartComponent,
-    NavBarComponent
+    NavBarComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/reviews/reviews.component.spec.ts
+++ src/app/reviews/reviews.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { ReviewsComponent } from './reviews.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('ReviewsComponent', () => {
+  let component: ReviewsComponent;
+  let fixture: ComponentFixture<ReviewsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      declarations: [ ReviewsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(ReviewsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

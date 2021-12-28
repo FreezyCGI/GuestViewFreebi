e19d602dc7a3488c37b241866d7a16c43ef2a18f
FreezyCGI <patrick.fritzl@gmx.at>
2021-12-28 19:07:39 +0100

    First Service implemented with api get request

--- src/app/models/menu-item-model.model.ts
+++ src/app/models/menu-item-model.model.ts
@@ -1,10 +1,10 @@
 export class MenuItemModel
 {
-    itemId: number;
-    title: string;
-    description: string;
-    price: number;
-    allergens: string;
-    status: string;
-    categories: number[];
+    itemid: number = 0;
+    title: string = "";
+    description: string = "";
+    price: number = 0;
+    allergens: string = "";
+    status: string = "";
+    categories: number[] = [];
 }

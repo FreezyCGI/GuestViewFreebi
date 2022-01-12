edd7d6f70adaf8d9948ccf0e0d471b1869726e9d
FreezyCGI <patrick.fritzl@gmx.at>
2022-01-12 16:57:33 +0100

    implemented review component with dummy values

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from '../models/review.model';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css']
 })
+
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviewList:Review[] = [];
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+    this.reviewList = [
+      {description:"desc1", reviewId:1, createdAt:new Date(), stars:2},
+      {description:"desc2", reviewId:2, createdAt:new Date(), stars:3}]
+  }
+
+  //Pfusch 
+  //Convert numbers to an array. 
+  //Example: input: 10
+  //         output: array[10] 
+  //needed for *ngFor loop of star-images
+  starsToArr(stars:number):number[]
+  {
+    let starArr:number[] = [];
+
+    for(let i = 0; i < stars; i++)
+    {
+      starArr.push(0);
+    }
+    return starArr;
   }
 
 }

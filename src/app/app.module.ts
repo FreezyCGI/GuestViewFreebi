c62ff396b81ec3de972a78af3ad9675940afee43
FreezyCGI <patrick.fritzl@gmx.at>
2022-01-17 00:25:30 +0100

    Deleted menu-item-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MenuItemComponent } from './menu-item/menu-item.component';
-import { MenuItemListComponent } from './menu-item-list/menu-item-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +26,6 @@ import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
   declarations: [
     AppComponent,
     MenuItemComponent,
-    MenuItemListComponent,
     ShoppingCartComponent,
     NavBarComponent,
     ReviewsComponent,

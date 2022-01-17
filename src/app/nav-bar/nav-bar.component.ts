cd62eb3d6b380d632279ae85881eaf8328539f25
FreezyCGI <patrick.fritzl@gmx.at>
2022-01-17 01:26:27 +0100

    Fixed nav-bar selected bug
    when changing url with routing, the selected nav-bar-element wouldnt reflect the changes

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit
+{
   links = [
-    {link: '/home', iconName: 'home', name: 'Home'}, 
-    {link: '/all', iconName: 'menu_book', name: 'Menu'}, 
-    {link: '/myOrders', iconName: 'shopping_bag', name: 'My Orders'}, 
-    {link: '/reviews', iconName: 'mode_edit', name: 'Reviews'}];
+    { link: '/home', iconName: 'home', name: 'Home' },
+    { link: '/all', iconName: 'menu_book', name: 'Menu' },
+    { link: '/myOrders', iconName: 'shopping_bag', name: 'My Orders' },
+    { link: '/reviews', iconName: 'mode_edit', name: 'Reviews' }];
   activeLink = {};
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+    this.router.events.subscribe((val) =>
+    {
+      this.setLink();
+    });
     this.setLink();
   }
 
   //Set Mat-tab-nav-bar to the right button in case of a site reload
-  setLink():void
+  setLink(): void
   {
-    this.links.forEach((link) => {
-      if(link.link == location.pathname)
+    this.links.forEach((link) =>
+    {
+      if (link.link == location.pathname)
       {
         this.activeLink = link;
       }

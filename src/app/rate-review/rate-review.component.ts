727a52d9c27f1ed8b4236353ee62a111c74dff47
ChibiChanXYZ <lisa.gassinger@gmx.at>
2022-02-02 21:30:37 +0100

    fixed bug with stars

--- src/app/rate-review/rate-review.component.ts
+++ src/app/rate-review/rate-review.component.ts
@@ -32,8 +32,6 @@ export class RateReviewComponent implements OnInit {
       this.activeStars[i] = false;
     }
     this.activeStars[0] = true;
-    this.activeStars[1] = true;
-    this.activeStars[2] = true;
   }
 
 }
